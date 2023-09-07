e9254af5b7db2ce07eced265300cd3c8eee3ceb0
noonever <noonevernowhere@gmail.com>
2023-09-07 06:44:35 +0300

    Multiform updated.

--- frontend/app/components/MultiForm.tsx
+++ frontend/app/components/MultiForm.tsx
@@ -1,227 +1,253 @@
-import React, { Component, ChangeEvent } from 'react';
+import React, { Component } from 'react';
+import type { ChangeEvent } from 'react';
 import './multiform-styles.css';
 
+type FieldConfig = {
+	type: string;
+	validation: RegExp;
+	placeholder?: string;
+	errorMessage?: string;
+	isRequired?: boolean;
+	defaultValue?: string;
+}
+
 interface FormTypeConfig {
-  maxAmount: number;
-  startAmount: number;
-  fields: Record<string, { type: string; validation: RegExp }>;
+	maxAmount: number;
+	startAmount: number;
+	fields: Record<string, FieldConfig>;
 }
 
 interface MultiFormProps {
-  formTypeConfigurations: Record<string, FormTypeConfig>;
-  className?: string;
+	formTypeConfigurations: Record<string, FormTypeConfig>;
+	className?: string;
+	onSubmitCallback?: (formData: Record<string, Record<number, Record<string, string>>>) => void;
 }
 
 interface MultiFormState {
-  formData: Record<string, Record<number, Record<string, { data: string; validity: boolean }>>>;
-  invalidFields: string[]; // Add the invalidFields property here
-
+	formData: Record<string, Record<number, Record<string, { data: string; validity: boolean }>>>;
+	invalidFields: string[]; // Add the invalidFields property here
 }
 
 class MultiForm extends Component<MultiFormProps, MultiFormState> {
-  constructor(props: MultiFormProps) {
-    super(props);
-    this.state = {
-      formData: this.initializeFormData(),
-      invalidFields: [],
-    };
-  }
+	constructor(props: MultiFormProps) {
+		super(props);
+		this.state = {
+			formData: this.initializeFormData(),
+			invalidFields: [],
+		};
+	}
 
-  initializeFormData(): Record<string, Record<number, Record<string, { data: string; validity: boolean }>>> {
-    const { formTypeConfigurations } = this.props;
-    const formData: Record<string, Record<number, Record<string, { data: string; validity: boolean }>>> = {};
+	initializeFormData(): Record<string, Record<number, Record<string, { data: string; validity: boolean }>>> {
+		const { formTypeConfigurations } = this.props;
+		const formData: Record<string, Record<number, Record<string, { data: string; validity: boolean }>>> = {};
 
-    Object.keys(formTypeConfigurations).forEach((formType) => {
-      formData[formType] = {};
-      for (let i = 1; i <= formTypeConfigurations[formType].startAmount; i++) {
-        formData[formType][i] = this.initializeFormFields(formType);
-      }
-    });
+		Object.keys(formTypeConfigurations).forEach((formType) => {
+			formData[formType] = {};
+			for (let i = 1; i <= formTypeConfigurations[formType].startAmount; i++) {
+				formData[formType][i] = this.initializeFormFields(formType);
+			}
+		});
 
-    return formData;
-  }
+		return formData;
+	}
 
-  initializeFormFields(formType: string): Record<string, { data: string; validity: boolean }> {
-    const { fields } = this.props.formTypeConfigurations[formType];
-    const formFields: Record<string, { data: string; validity: boolean }> = {};
+	initializeFormFields(formType: string): Record<string, { data: string; validity: boolean; }> {
+		const { fields } = this.props.formTypeConfigurations[formType];
+		const formFields: Record<string, { data: string; validity: boolean; }> = {};
 
-    Object.keys(fields).forEach((fieldAlias) => {
-      formFields[fieldAlias] = { data: '', validity: true };
-    });
+		Object.keys(fields).forEach((fieldAlias) => {
+			formFields[fieldAlias] = {
+				data: fields[fieldAlias].defaultValue || '',
+				validity: true,
+			};
+		});
 
-    return formFields;
-  }
+		return formFields;
+	}
 
-  handleInputChange = (
-    formType: string,
-    formIndex: number,
-    fieldAlias: string,
-    event: ChangeEvent<HTMLInputElement>
-  ) => {
-    const { value } = event.target;
+	handleInputChange = (
+		formType: string,
+		formIndex: number,
+		fieldAlias: string,
+		event: ChangeEvent<HTMLInputElement>
+	) => {
+		const { value } = event.target;
 
-    this.setState((prevState) => ({
-      formData: {
-        ...prevState.formData,
-        [formType]: {
-          ...prevState.formData[formType],
-          [formIndex]: {
-            ...prevState.formData[formType][formIndex],
-            [fieldAlias]: {
-              data: value,
-              validity: this.validateField(formType, fieldAlias, value),
-            },
-          },
-        },
-      },
-    }));
-  };
+		this.setState((prevState) => ({
+			formData: {
+				...prevState.formData,
+				[formType]: {
+					...prevState.formData[formType],
+					[formIndex]: {
+						...prevState.formData[formType][formIndex],
+						[fieldAlias]: {
+							data: value,
+							validity: this.validateField(formType, fieldAlias, value),
+						},
+					},
+				},
+			},
+		}));
+	};
 
-  validateField(formType: string, fieldAlias: string, value: string): boolean {
-    const { fields } = this.props.formTypeConfigurations[formType];
-    const regex = fields[fieldAlias].validation;
-    return regex.test(value);
-  }
+	validateField(formType: string, fieldAlias: string, value: string): boolean {
+		const { fields } = this.props.formTypeConfigurations[formType];
+		const regex = fields[fieldAlias].validation;
+		return regex.test(value);
+	}
 
-  handleAddForm = (formType: string) => {
-    const { maxAmount } = this.props.formTypeConfigurations[formType];
-    const formIndices = Object.keys(this.state.formData[formType]);
-    const newIndex = formIndices.length ? Math.max(...formIndices.map(Number)) + 1 : 1;
+	handleAddForm = (formType: string) => {
+		const { maxAmount } = this.props.formTypeConfigurations[formType];
+		const formIndices = Object.keys(this.state.formData[formType]);
+		const newIndex = formIndices.length ? Math.max(...formIndices.map(Number)) + 1 : 1;
 
-    if (formIndices.length < maxAmount) {
-      this.setState((prevState) => ({
-        formData: {
-          ...prevState.formData,
-          [formType]: {
-            ...prevState.formData[formType],
-            [newIndex]: this.initializeFormFields(formType),
-          },
-        },
-      }));
-    }
-  };
+		if (formIndices.length < maxAmount) {
+			this.setState((prevState) => ({
+				formData: {
+					...prevState.formData,
+					[formType]: {
+						...prevState.formData[formType],
+						[newIndex]: this.initializeFormFields(formType),
+					},
+				},
+			}));
+		}
+	};
 
-  handleDeleteForm = (formType: string, formIndex: number) => {
-    const formDataCopy = { ...this.state.formData };
-    delete formDataCopy[formType][formIndex];
+	handleDeleteForm = (formType: string, formIndex: number) => {
+		const formDataCopy = { ...this.state.formData };
+		delete formDataCopy[formType][formIndex];
 
-    this.setState({
-      formData: formDataCopy,
-    });
-  };
+		this.setState({
+			formData: formDataCopy,
+		});
+	};
 
-  handleSubmit = () => {
-    const { formData } = this.state;
-    let allFieldsValid = true; // Flag to check if all fields are valid
-  
-    // Check the validity of all fields and collect invalid field keys
-    const invalidFieldKeys = [];
-  
-    for (const formType in formData) {
-      for (const formIndex in formData[formType]) {
-        for (const fieldAlias in formData[formType][formIndex]) {
-          const inputField = formData[formType][formIndex][fieldAlias];
-          if (!inputField.validity) {
-            // Field is invalid
-            allFieldsValid = false;
-            invalidFieldKeys.push(`${formType}-${formIndex}-${fieldAlias}`);
-          }
-        }
-      }
-    }
-  
-    if (!allFieldsValid) {
-      // If any field is invalid, set invalidFields state to trigger re-render
-      this.setState({ invalidFields: invalidFieldKeys });
-      console.log(invalidFieldKeys);
-      // Remove the blinking class after 1 second (adjust as needed)
-      setTimeout(() => {
-        this.setState({ invalidFields: [] });
-      }, 1000);
-    }
-  
-    if (allFieldsValid) {
-      // All fields are valid, log data without "validity"
-      const dataWithoutValidity = {};
-  
-      for (const formType in formData) {
-        dataWithoutValidity[formType] = {};
-        for (const formIndex in formData[formType]) {
-          dataWithoutValidity[formType][formIndex] = {};
-          for (const fieldAlias in formData[formType][formIndex]) {
-            dataWithoutValidity[formType][formIndex][fieldAlias] =
-              formData[formType][formIndex][fieldAlias].data;
-          }
-        }
-      }
-  
-      console.log(dataWithoutValidity);
-    }
-  };
-  
-  
+	handleSubmit = () => {
+		const { formData } = this.state;
 
-render() {
-  const { formTypeConfigurations, className } = this.props;
-  const { formData, invalidFields } = this.state;
+		let allFieldsValid = true; // Flag to check if all fields are valid
 
-  return (
-    <div className={className}>
-      {Object.keys(formTypeConfigurations).map((formType) => (
-        <div key={formType} className={`${formType}-container`}>
-          {Object.keys(formData[formType]).map((formIndex, serial) => (
-            <div key={formIndex} className={`form-entry`}>
-              <p className="form-title">{formType + ' ' + (serial + 1)}</p>
-              {Object.keys(formTypeConfigurations[formType].fields).map((fieldAlias) => {
-                const inputField = formData[formType][formIndex][fieldAlias];
-                const isInvalid = !inputField.validity;
-                const fieldKey = `${formType}-${formIndex}-${fieldAlias}`;
+		// Check the validity of all fields and collect invalid field keys
+		const invalidFieldKeys = [];
 
-                // Add a conditional class based on invalidFields state
-                const inputClassName = `input-field ${isInvalid ? 'invalid' : ''} ${
-                  invalidFields.includes(fieldKey) ? 'invalid-blink' : ''
-                }`;
+		for (const formType in formData) {
+			for (const formIndex in formData[formType]) {
+				for (const fieldAlias in formData[formType][formIndex]) {
+					const inputField = formData[formType][formIndex][fieldAlias];
+					const validity = this.validateField(formType, fieldAlias, inputField.data);
+					const data = inputField.data;
 
-                return (
-                  <div key={fieldAlias} className={`form-field ${isInvalid ? 'invalid' : ''}`}>
-                    <label className="field-label">{fieldAlias}</label>
-                    <input
-                      type={formTypeConfigurations[formType].fields[fieldAlias].type}
-                      value={inputField.data}
-                      onChange={(event) =>
-                        this.handleInputChange(formType, parseInt(formIndex), fieldAlias, event)
-                      }
-                      className={inputClassName}
-                      data-field-key={fieldKey}
-                    />
-                  </div>
-                );
-              })}
-              {serial >= formTypeConfigurations[formType].startAmount && (
-                <button
-                  className="delete-button"
-                  onClick={() => this.handleDeleteForm(formType, parseInt(formIndex))}
-                >
-                  Delete
-                </button>
-              )}
-            </div>
-          ))}
-          {Object.keys(formData[formType]).length < formTypeConfigurations[formType].maxAmount && (
-            <button className="add-button" onClick={() => this.handleAddForm(formType)}>
-              Add {formType}
-            </button>
-          )}
-        </div>
-      ))}
-      <button className="submit-button" onClick={this.handleSubmit}>
-        Submit
-      </button>
-    </div>
-  );
+					const isRequired = this.props.formTypeConfigurations[formType].fields[fieldAlias].isRequired !== false;
+
+					if (!validity || (!data && isRequired)) {
+						console.log(`${formType} - ${formIndex} - ${fieldAlias}`);
+						allFieldsValid = false;
+						invalidFieldKeys.push(`${formType}-${formIndex}-${fieldAlias}`);
+					}
+				}
+			}
+		}
+
+		if (!allFieldsValid || invalidFieldKeys.length === 0) {
+			// If any field is invalid or the form is empty, set invalidFields state to trigger re-render
+			this.setState({ invalidFields: invalidFieldKeys });
+
+			// Remove the blinking class after 1 second (adjust as needed)
+			setTimeout(() => {
+				this.setState({ invalidFields: [] });
+			}, 1000);
+		}
+
+		if (allFieldsValid && invalidFieldKeys.length > 0) {
+			// All fields are valid, but the form is empty, do not log data
+			return;
+		}
+
+		if (allFieldsValid) {
+			// All fields are valid, log data without "validity"
+			const dataWithoutValidity: Record<string, Record<number, Record<string, string>>> = {};
+			for (const formType in formData) {
+				dataWithoutValidity[formType] = {};
+				for (const formIndex in formData[formType]) {
+					dataWithoutValidity[formType][formIndex] = {};
+					for (const fieldAlias in formData[formType][formIndex]) {
+						dataWithoutValidity[formType][formIndex][fieldAlias] =
+							formData[formType][formIndex][fieldAlias].data;
+					}
+				}
+			}
+			console.log(dataWithoutValidity);
+			if (this.props.onSubmitCallback) {
+				this.props.onSubmitCallback(dataWithoutValidity);
+			}
+		}
+	};
+
+	render() {
+		const { formTypeConfigurations } = this.props;
+		const { formData, invalidFields } = this.state;
+
+		return (
+			<div className="multi-form-container">
+				{Object.keys(formTypeConfigurations).map((formType) => (
+					<div key={formType} className={`block-container`}>
+						{Object.keys(formData[formType]).map((formIndex, serial) => (
+							<div key={formIndex} className={`form-entry`}>
+								<p className="form-title">{formType + ' ' + (serial + 1)}</p>
+								{Object.keys(formTypeConfigurations[formType].fields).map((fieldAlias) => {
+									const inputFieldState = formData[formType][Number(formIndex)][fieldAlias];
+									const isInvalid = !inputFieldState.validity;
+									const fieldKey = `${formType}-${formIndex}-${fieldAlias}`;
+									const placeholder = formTypeConfigurations[formType].fields[fieldAlias].placeholder;
+									const errorMessage = formTypeConfigurations[formType].fields[fieldAlias].errorMessage;
+
+									// Add a conditional class based on invalidFields state
+									const inputClassName = `input-field ${isInvalid ? 'invalid' : ''} ${invalidFields.includes(fieldKey) ? 'invalid-blink' : ''
+										}`;
+
+									return (
+										<div key={fieldAlias} className={`form-field ${isInvalid ? 'invalid' : ''}`}>
+											<label className="field-label">{fieldAlias}</label>
+											<input
+												type={formTypeConfigurations[formType].fields[fieldAlias].type}
+												value={inputFieldState.data}
+												onChange={(event) =>
+													this.handleInputChange(formType, parseInt(formIndex), fieldAlias, event)
+												}
+												className={inputClassName}
+												data-field-key={fieldKey}
+												placeholder={placeholder}
+											/>
+											{isInvalid && <div className="error-message">{errorMessage}</div>} {/* Display error message */}
+										</div>
+									);
+								})}
+								{serial >= formTypeConfigurations[formType].startAmount && (
+									<button
+										className="delete-button"
+										onClick={() => this.handleDeleteForm(formType, parseInt(formIndex))}
+									>
+										Delete
+									</button>
+								)}
+							</div>
+						))}
+						{Object.keys(formData[formType]).length < formTypeConfigurations[formType].maxAmount && (
+							<button className="add-button" onClick={() => this.handleAddForm(formType)}>
+								Add {formType}
+							</button>
+						)}
+					</div>
+				))}
+				<button className="submit-button" onClick={this.handleSubmit}>
+					Submit
+				</button>
+			</div>
+		);
+	}
+
 }
-
-}  
 
 export default MultiForm;
 export type { FormTypeConfig };
--- frontend/app/routes/1.tsx
+++ frontend/app/routes/1.tsx
@@ -9,43 +9,66 @@ export const meta: V2_MetaFunction = () => {
     ];
 };
 
-const formTypeConfigurations: Record<string, FormTypeConfig> = {
+
+  const labelReleaseContractFormConfig: Record<string, FormTypeConfig> = {
     Release: {
-        maxAmount: 1,
-        startAmount: 1,
-        fields: {
-            name: {
-                type: 'text',
-                validation: /^[A-Za-z\s]+$/,
-            },
-            age: {
-                type: 'number',
-                validation: /^\d+$/,
-            },
+      maxAmount: 1,
+      startAmount: 1,
+      fields: {
+        ReleaseTitle: {
+          type: "text",
+          validation: /^[a-zA-Z0-9\s-]*$/,
+          placeholder: "Enter Release Title",
+          errorMessage: "Release Title is required and should contain only letters, numbers, spaces, or hyphens.",
+          isRequired: true,
+          defaultValue: "Default Release Title", // Add defaultValue
         },
+        ReleaseDate: {
+          type: "date",
+          validation: /^\d{4}-\d{2}-\d{2}$/,
+          placeholder: "Select Release Date",
+          errorMessage: "Release Date is required and should be in the format YYYY-MM-DD.",
+          isRequired: true,
+          defaultValue: "2023-01-01", // Add defaultValue
+        },
+      },
     },
     Track: {
-        maxAmount: 10,
-        startAmount: 1,
-        fields: {
-            email: {
-                type: 'email',
-                validation: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
-            },
-            phone: {
-                type: 'tel',
-                validation: /^\d{10}$/,
-            },
+      maxAmount: 100,
+      startAmount: 1,
+      fields: {
+        TrackTitle: {
+          type: "text",
+          validation: /^[a-zA-Z0-9\s-]*$/,
+          placeholder: "Enter Track Title",
+          errorMessage: "Track Title is required and should contain only letters, numbers, spaces, or hyphens.",
+          isRequired: true,
+          defaultValue: "Default Track Title", // Add defaultValue
         },
+        TrackArtist: {
+          type: "text",
+          validation: /^[a-zA-Z0-9\s-]*$/,
+          placeholder: "Enter Track Artist",
+          errorMessage: "Track Artist is required and should contain only letters, numbers, spaces, or hyphens.",
+          isRequired: true,
+          defaultValue: "Default Track Artist", // Add defaultValue
+        },
+        Duration: {
+          type: "text",
+          validation: /^[a-zA-Z0-9\s-]*$/,
+          placeholder: "Enter Track Duration",
+          errorMessage: "Track Duration is required and should contain only letters, numbers, spaces, or hyphens.",
+          isRequired: true,
+          defaultValue: "00:00", // Add defaultValue
+        }
+      },
     },
-    // Add more form type configurations as needed
-};
+  };
 
 export default function Index() {
     return (
         <div className="app-container mx-auto mt-5 grid w-full max-w-sm items-center gap-1.5">
-            <MultiForm formTypeConfigurations={formTypeConfigurations}/>
+            <MultiForm formTypeConfigurations={labelReleaseContractFormConfig}/>
         </div>
     );
 }
-
