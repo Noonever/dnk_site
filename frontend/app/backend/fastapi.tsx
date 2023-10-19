618b0c80a884b51941078e5ae47ccc55541a8d76
noonever <noonevernowhere@gmail.com>
2023-10-19 11:38:37 +0300

    Server ip

--- frontend/app/backend/fastapi.tsx
+++ frontend/app/backend/fastapi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fastAPI = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'http://185.234.10.244',
     timeout: 20000,
     headers: { 'X-Custom-Header': 'foobar' }
 });
